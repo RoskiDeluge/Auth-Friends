544f9d227806013d8f5069ca4c7a792fdd806db4
RoskiDeluge <roberto.delgado@gmail.com>
2020-02-17 09:46:21 -1000

    getData axiosWithAuth request working

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 
 const FriendsList = () => {
+    const [ friends, setFriends ] = useState([]);
+
+    useEffect(() => {
+        getData();
+    }, [])
+
+    const getData = () => {
+        axiosWithAuth()
+        .get("/friends")
+        .then(res => {
+            console.log(res);
+            setFriends(res.data)
+        })
+        .catch(err => console.error(err));
+    };
 
     return (
-        <div>I'm a friends list</div>
+        <div>
+            {friends.map(friend => {
+                return <div>{friend.name}</div>
+            })}
+        </div>
     )
 }
 
